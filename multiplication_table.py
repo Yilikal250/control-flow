7326d5bc9a73674a7651295da6cce5af900a806a
Yilikal250 <teshomeyilikal250@gmail.com>
2024-06-24 11:19:48 +0200

    rectangle pattern

--- multiplication_table.py
+++ multiplication_table.py
@@ -1,3 +1,3 @@
 number = int(input("Enter a number to see its multiplication table: "))
-for num in range(1, 11):
+for num in range(1, 5):
     print(str(num) + " * " + str(number) + " = " + str(num * number))
